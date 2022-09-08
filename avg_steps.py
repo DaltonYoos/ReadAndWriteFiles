ff2fac6b3feaba4f67a42e18ec65f370b7d415a7
Dalton <DY@test.com>
2022-09-08 12:27:48 -0500

    Working on final step

--- avg_steps.py
+++ avg_steps.py
@@ -3,9 +3,37 @@ import csv
 infile = open('steps.csv', 'r')
 outfile = open ('avg_steps.csv', 'w')
 
+Months = ["January", "February", "March", "April", "May", "June", "July", "August", "Spetember", "October","November", "December"]
+Counter_Months = 1
+
 csvfile = csv.reader(infile, delimiter=',')
 
 next(csvfile)
 
-outfile.write("Month, Avg Steps\n")
+outfile.write("Month, Average Steps\n")
+
+
+
+for Records in csvfile:
+
+   
+
+
+    if Months[0] == 1:
+
+        JanSteps = (Steps/Days_In_Month)
+        JanSteps = str(JanSteps)
+
+        outfile.write(JanSteps)
+
+
+
+
+
+
+
+
+
+
+    
 
