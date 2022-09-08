f41165d8904ee48c952beb586156e9319bf5be52
Dalton <DY@test.com>
2022-09-08 18:12:35 -0500

    Completed Assignment

--- avg_steps.py
+++ avg_steps.py
@@ -3,37 +3,43 @@ import csv
 infile = open('steps.csv', 'r')
 outfile = open ('avg_steps.csv', 'w')
 
-Months = ["January", "February", "March", "April", "May", "June", "July", "August", "Spetember", "October","November", "December"]
-Counter_Months = 1
-
 csvfile = csv.reader(infile, delimiter=',')
 
 next(csvfile)
 
-outfile.write("Month, Average Steps\n")
+
+outfile.write('Month, Average Steps\n')
+
+Months_Of_Year = ['Months:','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+Month = 1
+Step_Counter = 0
+Days_In_Month = 0
 
 
 
-for Records in csvfile:
+for Record in csvfile:
 
-   
+    if Record[0] == str(Month):
 
+        Step_Counter += int(Record[1])
 
-    if Months[0] == 1:
+        Days_In_Month += 1
+    
+    else:
 
-        JanSteps = (Steps/Days_In_Month)
-        JanSteps = str(JanSteps)
+        Average_Steps_Month = format(Step_Counter/Days_In_Month, '.2f')
 
-        outfile.write(JanSteps)
+        outfile.write(Months_Of_Year[int(Month)] + ', ' + str(Average_Steps_Month) + '\n')
 
+        Month = Record[0]
 
+        Step_Counter = int(Record[1])
 
-
-
-
-
-
-
+        Days_In_Month = 1
 
     
 
+outfile.close()
+        
